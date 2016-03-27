c80bcd66e450a756ec737ec7a3e01ee198e5e8e9
shuming2 <Amber@uws103695usr.uws.ualberta.ca>
2016-03-27 14:36:14 -0600

    Connection Failure

--- src/CreateTable.java
+++ src/CreateTable.java
@@ -6,12 +6,15 @@ import com.datastax.driver.core.Cluster;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class CreateTable {
     public static void main(String[] args) {
+        String create = "";
 
-        //Cluster cluster = Cluster.builder().withPort(9201).addContactPoint("10.2.3.12").build();
-        Cluster cluster = Cluster.builder().withPort(9201).addContactPoint("162.246.157.170").build();
+        Cluster cluster = Cluster.builder().withPort(9101).addContactPoint("10.2.2.143").build();
+        //Cluster cluster = Cluster.builder().withPort(9201).addContactPoint("162.246.157.170").build();
 
         Session session = cluster.connect();
 
@@ -20,94 +23,31 @@ public class CreateTable {
         Session kpSession = cluster.connect("mykeyspace");
 
 
+        // transfer the sql to string
         try {
-            InputStream r = new FileInputStream("bigdata_setup1.sql");
+            InputStream r = new FileInputStream("src/bigdata_setup1.sql");
             ByteArrayOutputStream byteout = new ByteArrayOutputStream();
-            byte tmp[] = new byte[256];
+            byte tmp[] = new byte[1];
             byte context[];
             int i = 0;
             while ((i = r.read(tmp)) != -1) {
                 byteout.write(tmp);
             }
             context = byteout.toByteArray();
-            String str = new String(context, "UTF-8");
+            create = new String(context, "UTF-8");
 
-            kpSession.execute(str);
+
+            kpSession.execute(create);
         } catch (Exception e) {
             System.out.println("Error: Creating table fails");
         }
 
-        kpSession.close();
-        session.close();
-        cluster.close();
-    }
-
-
-
-
-    /*
-    public static void main(String[] args) {
-        QueryOptions options = new QueryOptions();
-        options.setConsistencyLevel(ConsistencyLevel.QUORUM);
-
-        Cluster cluster = Cluster.builder()
-                .addContactPoint("10.2.3.12")   //seed node
-                .withCredentials("cassandra", "cassandra")
-                .withQueryOptions(options)
-                .build();
-
-        Session session = cluster.connect();
-        session.execute("CREATE KEYSPACE kp WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };");
-
-        Session kpSession = cluster.connect("kp");
-
-
-
-
-        kpSession.execute("CREATE TABLE tbl(a INT,  b INT, c INT, PRIMARY KEY(a));");
-
-        RegularStatement insert = QueryBuilder.insertInto("kp", "tbl").values(new String[]{"a", "b", "c"}, new Object[]{1, 2, 3});
-        kpSession.execute(insert);
-
-        RegularStatement insert2 = QueryBuilder.insertInto("kp", "tbl").values(new String[]{"a", "b", "c"}, new Object[]{3, 2, 1});
-        kpSession.execute(insert2);
-
-        RegularStatement delete = QueryBuilder.delete().from("kp", "tbl").where(QueryBuilder.eq("a", 1));
-        kpSession.execute(delete);
-
-        RegularStatement update = QueryBuilder.update("kp", "tbl").with(QueryBuilder.set("b", 6)).where(QueryBuilder.eq("a", 3));
-        kpSession.execute(update);
-
-        RegularStatement select = QueryBuilder.select().from("kp", "tbl").where(QueryBuilder.eq("a", 3));
-        ResultSet rs = kpSession.execute(select);
-        Iterator<Row> iterator = rs.iterator();
-        while (iterator.hasNext()) {
-            Row row = iterator.next();
-            System.out.println("a=" + row.getInt("a"));
-            System.out.println("b=" + row.getInt("b"));
-            System.out.println("c=" + row.getInt("c"));
-        }
-
-
-        //~~~~~~~~~~
-        ResultSet results = session.execute("SELECT * FROM simplex.playlists "
-                + "WHERE id = 2cc9ccb7-6221-4ccb-8387-f22b6a1b354d;");
-        // + "WHERE id = 2cc9ccb7-6221-4ccb-8387-f22b6a1b354d;");
-        System.out.println(String.format("%-30s\t%-20s\t%-20s\n%s", "title", "album", "artist",
-                "-------------------------------+-----------------------+--------------------"));
-        for (Row row : results) {
-            System.out.println(String.format("%-30s\t%-20s\t%-20s", row.getString("title"), row.getString("album"),
-                    row.getString("artist")));
-        }
-        System.out.println();
-        //~~~~~~~~~~~~
 
         kpSession.close();
         session.close();
         cluster.close();
+
     }
 
-
-    */
 
 }
--- src/InsertTable.java
+++ src/InsertTable.java
@@ -6,10 +6,14 @@ import com.datastax.driver.core.querybuilder.QueryBuilder;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class InsertTable {
     public static void main(String[] args) {
-
+        String str ="";
         Cluster cluster = Cluster.builder().addContactPoint("10.2.3.12").build();
         Session session = cluster.connect();
 
@@ -20,9 +24,62 @@ public class InsertTable {
         RegularStatement insert = QueryBuilder.insertInto("mykeyspace", "cdr").values(new String[]{"a", "b", "c"}, new Object[]{1, 2, 3});
         kpSession.execute(insert);
 
+        // transfer the sql to string
+        try {
+            InputStream r = new FileInputStream("src/bigdata_setup1.sql");
+            ByteArrayOutputStream byteout = new ByteArrayOutputStream();
+            byte tmp[] = new byte[1];
+            byte context[];
+            int i = 0;
+            while ((i = r.read(tmp)) != -1) {
+                byteout.write(tmp);
+            }
+            context = byteout.toByteArray();
+            str = new String(context, "UTF-8");
+        } catch (Exception e) {
+            System.out.println("Error: File does not exist");
+        }
+
+
+        Map<String, String> columns = getColumns(str);
+
 
         kpSession.close();
         session.close();
         cluster.close();
+    }
+
+
+    private static Map<String, String> getColumns (String str) {
+        Map<String, String> map = new LinkedHashMap<>();
+
+
+        str = str.substring(str.indexOf("(") + 1, str.lastIndexOf(")"));
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(str.split(",")));
+        list.remove(list.size()-1);
+
+        String[] items;
+        int i;
+        for (String item: list) {
+            items = item.split(" ");
+            for (i = 1; i < items.length; i++) {
+                if (!items[i].equals(" ") && !items[i].equals("")) {
+                    break;
+                }
+            }
+            map.put(items[0],items[i]);
+        }
+
+        return map;
+    }
+
+    private String populate(Map<String, String> columns) {
+        for(String key: columns.keySet()) {
+
+        }
+
+
+
+        return null;
     }
 }
